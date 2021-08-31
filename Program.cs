ccc5047e545fba7d7e77d0dd666dfe50e61481b7
Sujit Bagade <sujitbagade2@gmail.com>
2021-08-31 23:01:36 +0530

    Gotolabel

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace gotolabel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int i = 3, j = 0;
+            switch (i)
+            {
+                case 1:
+                    j += 10;
+                    Console.WriteLine("j value is {0}", j);
+                    break;
+                case 2:
+                    j += 5;
+                    goto case 1;
+                case 3:
+                    j += 10;
+                    goto case 1;
+                default:
+                    Console.WriteLine("");
+                    break;
+            }
+            Console.WriteLine("Press Enter Key to Exit..");
+            Console.ReadLine();
+        }
+    }
+    
+}
